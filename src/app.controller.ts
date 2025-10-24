da7b512b4cc59c733a5036cd9c69a83fb6ace875
Trường Nguyễn Vỹ <truongton2k4dz@gmail.com>
2025-10-24 17:40:05 +0700

    test get and post

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
-export class AppController {
+@Controller('hello')
+export class HelloController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+}
+
+@Controller('users')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  findAll() {
+    return this.appService.findAll();
+  }
+
+  @Post() 
+  postUser() {
+    return this.appService.postUser();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,47 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
+export class AppHello {
+
   getHello(): string {
     return 'Hello World12344!';
+  }
+}
+
+export class AppService {
+  //TODO: get() post() put()/patch() delete()
+
+  private data = [
+    {
+      id: 1,
+      name: "TruongNV",
+      dob: "",
+      city: ""
+    },
+    {
+      id: 2,
+      name: "TruongNV2",
+      dob: "",
+      city: ""
+    }
+  ];
+
+  getHello(): string {
+    return 'Hello World12344!';
+  }
+
+  findAll() {
+    return this.data;
+  }
+
+  postUser() {
+    const newObject = { id: 3, name: "Test3", dob: "", city: ""};
+    this.data.push(newObject);
+    console.log(this.data);
+    return this.data;
+
+    //  const newObject = { id: 3, name: "Test3", dob: "", city: ""};
+    // let newObject2 = this.data.concat(newObject)
+    // return newObject2;
   }
 }
